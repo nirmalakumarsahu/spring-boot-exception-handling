b9051e1e97132d805d89097de4b20db52f669ffb
Nirmala Kumar Sahu <nirmalakumarsahu7@gmail.com>
2025-08-26 23:21:48 +0530

    Api Response and ProductUtil

---
+++ src/main/java/com/sahu/springboot/basics/service/util/ProductUtil.java
@@ -0,0 +1,28 @@
+package com.sahu.springboot.basics.service.util;
+
+import com.sahu.springboot.basics.dto.ProductRequest;
+import com.sahu.springboot.basics.dto.ProductResponse;
+import com.sahu.springboot.basics.model.Product;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class ProductUtil {
+
+    public ProductResponse toProductResponse(Product product) {
+        return ProductResponse.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .description(product.getDescription())
+                .price(product.getPrice())
+                .build();
+    }
+
+    public Product toProduct(ProductRequest productRequest) {
+        return Product.builder()
+                .name(productRequest.name())
+                .description(productRequest.description())
+                .price(productRequest.price())
+                .build();
+    }
+
+}
